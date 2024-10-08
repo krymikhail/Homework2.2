1135a8a71028db64e7c16fdaab77cf4f4eed187a
krymikhail <blackmiha5@gmail.com>
2024-10-08 16:43:05 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> listA = new ArrayList<>();
+        System.out.println("Введите 5 строк для списка A:");
+        for (int i = 0; i < 5; i++) {
+            listA.add(scanner.nextLine());
+        }
+
+        System.out.println("Список A: " + listA);
+
+        ArrayList<String> listB = new ArrayList<>();
+        System.out.println("Введите 5 строк для списка B:");
+        for (int i = 0; i < 5; i++) {
+            listB.add(scanner.nextLine());
+        }
+
+        System.out.println("Список B: " + listB);
+
+        ArrayList<String> listC = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            listC.add(listA.get(i));
+            listC.add(listB.get(4 - i));
+        }
+
+        System.out.println("Список C после объединения: " + listC);
+
+        Collections.sort(listC, Comparator.comparingInt(String::length));
+
+        System.out.println("Список C, отсортированный по длине: " + listC);
+    }
+}
